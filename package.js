bfff6461b4adde17539d25f0aa81bd145660ec29
andrej fritz <a.fritz@betzold.de>
2014-09-10 16:29:03 +0200

    updated to meteor 0.9

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-  summary: "ScrolltoFixed.js, fix elements on the page"
+  summary: "ScrolltoFixed.js, fix elements on the page",
+  version: "0.0.2",
+  git: "https://github.com/andruschka/meteor-scroll-to-fixed.git"
 });
 
 Package.on_use(function(api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.use(['jquery'], 'client');
   api.add_files(
     [
